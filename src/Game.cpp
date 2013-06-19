92c83e84205878bc4dc72404831e00334002090c
Esa Varemo <esa@kuivanto.fi>
2013-06-19 22:21:27 +0300

    Load the stage

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,11 @@
+#include <fstream>
+#include <iostream>
+#include <exception>
+#include <string>
+#include <limits>
+
 #include "Game.h"
+#include "Ground.h"
 
 Game::Game() {
 
@@ -10,4 +17,27 @@ std::vector<Entity*>* Game::getEntities() {
 	entities->insert(entities->end(), ground->begin(), ground->end());
 
 	return entities;
+}
+
+void Game::init() {
+	initGround();
+}
+
+void Game::initGround() {
+	std::ifstream *stageFile = new std::ifstream();
+	stageFile->open("stage.txt");
+
+	if(stageFile == nullptr ){//|| !stageFile->is_open()) {
+		printf("Stage-file not found or could not open");
+		throw std::exception();
+	}
+
+	std::string line;
+	float *x, *y, *w, *h;
+	while(std::getline(*stageFile, line)) {
+		sscanf(line.c_str(), "%f %f %f %f", x, y, w, h);
+		ground->push_back(new Ground(*x, *y ,*w, *h));
+	}
+
+
 }
--- src/Ground.cpp
+++ src/Ground.cpp
@@ -7,6 +7,13 @@
 
 #include "Ground.h"
 
+Ground::Ground(float x, float y, float w, float h) {
+	this->x = x;
+	this->y = y;
+	this->width = w;
+	this->height = h;
+}
+
 Ground::Ground() {
 
 }
