1242d609cf885560746124870d52c5eeedc1f293
Esa Varemo <esa@kuivanto.fi>
2013-06-20 02:17:51 +0300

    Added simple box-drawing

--- src/Ground.cpp
+++ src/Ground.cpp
@@ -5,7 +5,12 @@
  *      Author: esa
  */
 
+
+#include <iostream>
 #include "Ground.h"
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
 
 Ground::Ground(float x, float y, float w, float h) {
 	this->x = x;
@@ -16,6 +21,42 @@ Ground::Ground(float x, float y, float w, float h) {
 
 Ground::Ground() {
 
+}
+
+void Ground::draw(long deltaT) {
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glBegin(GL_QUADS);
+		glVertex3f(x+width, y+height, 0);
+		glVertex3f(x, y+height, 0);
+		glVertex3f(x, y, 0);
+		glVertex3f(x+width, y, 0);
+		glVertex3f(x+width, y+height, 0);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glVertex3f(x+width, y+height, -128);
+		glVertex3f(x+width, y+height, 0);
+		glVertex3f(x+width, y, 0);
+		glVertex3f(x+width, y, -128);
+		glVertex3f(x+width, y+height, -128);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glVertex3f(x, y+height, -128);
+		glVertex3f(x, y+height, 0);
+		glVertex3f(x, y, 0);
+		glVertex3f(x, y, -128);
+		glVertex3f(x, y+height, -128);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glVertex3f(x+width, y+height, -128);
+		glVertex3f(x, y+height, -128);
+		glVertex3f(x, y, -128);
+		glVertex3f(x+width, y, -128);
+		glVertex3f(x+width, y+height, -128);
+	glEnd();
+	std::cout << "Drawing!";
 }
 
 
